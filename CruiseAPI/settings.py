66deba87f6845ae7604d6c654449581a63c75563
Michael Ajanaku <remiljw@gmail.com>
2020-02-14 07:23:16 +0100

    Config

--- CruiseAPI/settings.py
+++ CruiseAPI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b!+zny1=nnon5_js#h&xzue8^6!rqy%cw8*qehf@ec4ni8s60v'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'b!+zny1=nnon5_js#h&xzue8^6!rqy%cw8*qehf@ec4ni8s60v')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = ['*']
 
@@ -82,11 +83,11 @@ WSGI_APPLICATION = 'CruiseAPI.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'cruise',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',   #
-        'PORT': '3308',
+        'NAME': config('DATABASE_NAME','cruise'),
+        'USER': config('DATABASE_USER', 'root'),
+        'PASSWORD':  config('DATABASE_PASSWORD', ''),
+        'HOST': config('DATABASE_HOST', 'localhost'),
+        'PORT':  config('DATABASE_PORT', '3308'),
     },
         'TEST': {
             'CHARSET': 'utf8mb4',
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # CruiseX
+This is a simulation of a Cruise Excursion API with CRUD options, optimized using sql queries rather than ORM.
+
+
+## Instuctions
+ - Clone repo 
+ - Navigate into the root directory and run `pip install -r requirements.txt` via the Terminal
+ - Edit the DATABASE in the _settings.py_ file
+ - run `python manage.py makemigrations` then `python mannage.py migrate` to move all data tables to your        database.
+ - Create a superuser `python manage.py createsuperuser`
+ - run `python manage.py runserver`
+ - Test your endpoints
+ 
